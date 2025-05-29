2801ab9a238e70be3e5b6e052cf21720eec6c63a
jetshree_sharma <135634085+Jetshree@users.noreply.github.com>
2025-05-29 09:50:35 +0530

    Create MyApp.js

---
+++ React/MyApp.js
@@ -0,0 +1,11 @@
+import Task from './Task.js'
+function MyApp(){
+    return (
+        <>
+        <h1>Hello</h1>
+        <h2>Jetshree</h2>
+        <Task />
+        </>
+    )
+}
+export default MyApp;
